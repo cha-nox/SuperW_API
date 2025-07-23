0dc8bf95d9f2959b4296fb99f11cad3b72748e04
FrozenCell239 <val8ioks@gmail.com>
2025-07-23 11:50:58 +0200

    Project addition - Added product list route.

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,8 +1,19 @@
-import { Router } from "express";
+import { Router }       from "express";
 import { PrismaClient } from '@prisma/client';
 
 const router = Router();
 const prisma = new PrismaClient();
+
+// Product list route
+const productList = async (req, res, next) => {
+    const product_list = await prisma.product.findMany();
+
+    res.json({
+        message:        "Liste des produits",
+        data_content:   product_list
+    });
+};
+router.get("/list", productList);
 
 // Exporting the routes
 export default router;
