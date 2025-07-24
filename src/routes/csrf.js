1a1615aa18815cf97be8a0bb99407c30ba3c01e9
FrozenCell239 <val8ioks@gmail.com>
2025-07-24 09:57:17 +0200

    Security fix - Fixed the HTTP method for the CSRF token generation route.

--- src/routes/csrf.js
+++ src/routes/csrf.js
@@ -4,7 +4,7 @@ import { generateCsrfToken }    from '../middlewares/csrfToken.js';
 const router = Router();
 
 // CSRF token generation route
-router.get('/generate_token', generateCsrfToken, (req, res) => {
+router.post('/generate_token', generateCsrfToken, (req, res) => {
     return res.status(200).json({csrf_token: res.locals.csrf_token});
 });
 
